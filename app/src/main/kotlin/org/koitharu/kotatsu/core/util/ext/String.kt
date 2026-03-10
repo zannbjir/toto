afdf825998950c95d0eb4d6fc59f0f319577b609
Razan Muhammad Ikhsan <raazan.muhammad.ikhsan@gmail.com>
2026-03-10 08:47:32 +0700

    Update String.kt

--- app/src/main/kotlin/org/koitharu/kotatsu/core/util/ext/String.kt
+++ app/src/main/kotlin/org/koitharu/kotatsu/core/util/ext/String.kt
@@ -72,7 +72,7 @@ fun <T> Collection<T>.joinToStringWithLimit(context: Context, limit: Int, transf
 
 fun String.isHttpUrl() = startsWith("https://", ignoreCase = true) || startsWith("http://", ignoreCase = true)
 
-fun String.isAnimatedImage() = false
+fun String.isAnimatedImage() = endsWith(".gif", ignoreCase = true) || endsWith(".webp", ignoreCase = true)
 
 fun concatStrings(context: Context, a: String?, b: String?): String? = when {
 	a.isNullOrEmpty() && b.isNullOrEmpty() -> null
